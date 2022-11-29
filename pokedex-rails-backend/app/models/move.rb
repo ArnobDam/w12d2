fc52a24cdec76e34a8017dd747f35b20dd6d14f5
Arnob <arnobdam@gmail.com>
2022-11-29 14:35:46 -0500

    added validations

--- pokedex-rails-backend/app/models/move.rb
+++ pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
+
+    validates :name, presence: true
 end
--- pokedex-rails-backend/app/models/pokemon.rb
+++ pokedex-rails-backend/app/models/pokemon.rb
@@ -1,2 +1,46 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+
+    TYPES = [
+        'fire',
+        'electric',
+        'normal',
+        'ghost',
+        'psychic',
+        'water',
+        'bug',
+        'dragon',
+        'grass',
+        'fighting',
+        'ice',
+        'flying',
+        'poison',
+        'ground',
+        'rock',
+        'steel'
+      ].sort.freeze
+    
+      # ...
+      validates :poke_type, inclusion: { in: TYPES, message: "'%{value}' is not a valid Pokemon type" }
+
+    validates :number, :name, :attack, :defense, :poke_type, :image_url, presence: true
+    validates :captured, inclusion: [true, false]
+    validates :name, length: { in: 3..255}, uniquness: { message: "'%{value}' is already in use" }
+    validates :number, uniquness: { message: "'%{value}' is already in use" }, numericality: { greater_than: 0 }
+    validates :attack, :defense, numericality: { in: 0..100 }
+
 end
